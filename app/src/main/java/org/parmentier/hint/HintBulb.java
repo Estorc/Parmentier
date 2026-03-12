7cf89a4538998c09ca9d2506b4d99bad0e57bace
Estorc <loup.estorc@gmail.com>
2026-03-12 13:58:17 +0100

    Changed hints

--- app/src/main/java/org/parmentier/hint/HintBulb.java
+++ app/src/main/java/org/parmentier/hint/HintBulb.java
@@ -29,16 +29,10 @@ public class HintBulb {
 
     static private HintBulb create() {
        return new HintBulb(List.of(
-            new Hint("Si une île n’a qu’un seul voisin, alors tous les ponts requis doivent être placés vers ce voisin." +
-                "Une fois ces ponts posés, l’île est complétée et peut être marquée comme terminée.", 10, 5, (grid) -> {
-                return grid.getNodes().stream()
-                    .filter(node -> node.getBridges().size() == 1) // Only consider islands with one neighbor
-                    .anyMatch(node -> {
-                        int requiredBridges = node.getValue(); // Get the required number of bridges for this island
-                        int existingBridges = node.getBridges().getFirst().getState();                        
-                          return existingBridges < requiredBridges; // Check if there are still bridges needed
-                    });
-            }),
+            /*
+             * Starting techniques
+             */
+
             new Hint("Une île placée dans un coin ne peut avoir que deux voisins." +
                 "Comme un lien ne peut contenir au plus que " + Bridge.MAX_STATE + " ponts, une île " + Bridge.MAX_STATE * 2 + " dans un coin doit envoyer deux ponts vers chacun de ses deux voisins." +
                 "L’île est alors complète.", 15, 3, (grid) -> {
@@ -63,6 +57,55 @@ public class HintBulb {
                 }),
             new Hint("Une île au centre peut avoir quatre voisins." +
                 "Une île " + Bridge.MAX_STATE * 4 + " doit donc envoyer " + Bridge.MAX_STATE + " ponts vers chacun de ses quatre voisins." +
+                "L’île est alors complète.", 25, 1, (grid) -> {
+                return grid.getNodes().stream()
+                    .filter(node -> grid.isCenter(node) && node.getValue() == Bridge.MAX_STATE * 4) // Only consider islands in corners
+                    .anyMatch(node -> {
+                        int requiredBridges = node.getValue(); // Get the required number of bridges for this island
+                        int existingBridges = node.getBridges().stream().reduce(0, (sum, bridge) -> sum + bridge.getState(), Integer::sum); // Get the number of existing bridges
+                        return existingBridges < requiredBridges; // Check if there are still bridges needed
+                    });
+                }),
+
+            /*
+             * Basic techniques
+             */
+
+            new Hint("Si une île n’a qu’un seul voisin, alors tous les ponts requis doivent être placés vers ce voisin." +
+                "Une fois ces ponts posés, l’île est complétée et peut être marquée comme terminée.", 10, 5, (grid) -> {
+                return grid.getNodes().stream()
+                    .filter(node -> node.getBridges().size() == 1) // Only consider islands with one neighbor
+                    .anyMatch(node -> {
+                        int requiredBridges = node.getValue(); // Get the required number of bridges for this island
+                        int existingBridges = node.getBridges().getFirst().getState();                        
+                          return existingBridges < requiredBridges; // Check if there are still bridges needed
+                    });
+            }),
+
+            new Hint("Une île placée dans un coin ne peut avoir que deux voisins." +
+                "Comme un lien ne peut contenir au plus que " + Bridge.MAX_STATE + " ponts, une île " + (Bridge.MAX_STATE * 2 - 1) + " dans un coin doit envoyer au moins un pont vers chacun de ses deux voisins." +
+                "L’île est alors complète.", 15, 3, (grid) -> {
+                return grid.getNodes().stream()
+                    .filter(node -> grid.isCorner(node) && node.getValue() == Bridge.MAX_STATE * 2) // Only consider islands in corners
+                    .anyMatch(node -> {
+                        int requiredBridges = node.getValue(); // Get the required number of bridges for this island
+                        int existingBridges = node.getBridges().stream().reduce(0, (sum, bridge) -> sum + bridge.getState(), Integer::sum); // Get the total number of existing bridges
+                        return existingBridges < requiredBridges; // Check if there are still bridges needed
+                    });
+                }),
+            new Hint("Une île située sur un bord peut avoir trois voisins." +
+                "Une île " + (Bridge.MAX_STATE * 3 - 1) + " sur un bord doit donc envoyer au moins un pont vers chacun de ses trois voisins." +
+                "L’île est alors complète.", 20, 2, (grid) -> {
+                return grid.getNodes().stream()
+                    .filter(node -> grid.isEdge(node) && node.getValue() == Bridge.MAX_STATE * 3) // Only consider islands in corners
+                    .anyMatch(node -> {
+                        int requiredBridges = node.getValue(); // Get the required number of bridges for this island
+                        int existingBridges = node.getBridges().stream().reduce(0, (sum, bridge) -> sum + bridge.getState(), Integer::sum); // Get the number of existing bridges
+                        return existingBridges < requiredBridges; // Check if there are still bridges needed
+                    });
+                }),
+            new Hint("Une île au centre peut avoir quatre voisins." +
+                "Une île " + (Bridge.MAX_STATE * 4 - 1) + " doit donc envoyer au moins un pont vers chacun de ses quatre voisins." +
                 "L’île est alors complète.", 25, 1, (grid) -> {
                 return grid.getNodes().stream()
                     .filter(node -> grid.isCenter(node) && node.getValue() == Bridge.MAX_STATE * 4) // Only consider islands in corners
